f80969aef3dd31e752f469be082f15853264d087
Ushan Rashmika <ushanrashmika23@gmail.com>
2025-11-20 23:59:33 +0530

    done

---
+++ main.js
@@ -0,0 +1,5 @@
+const flipInner = document.getElementById("flipInner");
+const flipBtn = document.getElementById("flipBtn");
+flipBtn.onclick = () => flipInner.classList.toggle("flipped");
+
+lucide.createIcons();
